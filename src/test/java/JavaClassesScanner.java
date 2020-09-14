d27066d9382007d5ee848e2dee12283ac06db018
Anu Luther <anu@chn-aluther.local>
2020-09-13 23:27:19 -0700

    Scannerclass

--- src/test/java/JavaClassesScanner.java
+++ src/test/java/JavaClassesScanner.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class JavaClasses {
+public class JavaClassesScanner {
 
     public static void main(String[] args) {
 // interface between user and progr for user input
